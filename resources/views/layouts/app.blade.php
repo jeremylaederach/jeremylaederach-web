3cd1055a3f65a6b44a5eb25642c3992bd9f5fd53
Jeremy Läderach <jeremy.kai.laederach@gmail.com>
2026-07-28 01:32:38 +0200

    refactor(brand): balance route color system

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="{{ $description }}">
-        <meta name="theme-color" content="#08070d">
+        <meta name="theme-color" content="#07070a">
 
         <title>{{ $pageTitle }}</title>
 
